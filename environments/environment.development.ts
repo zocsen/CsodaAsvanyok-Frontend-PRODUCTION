fa8e0dfba54121db368a93f0face65f4106b9c71
zocsen <zocsen@gmail.com>
2023-03-02 12:43:29 +0100

    asd

--- environments/environment.development.ts
+++ environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiURL: 'https://csodaasvanyok.up.railway.app/api/v1',
+	apiURL: 'http://csodaasvanyok.up.railway.app/api/v1',
 };
--- environments/environment.ts
+++ environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: false,
-	apiURL: 'https://csodaasvanyok.up.railway.app/api/v1'
+	apiURL: 'http://csodaasvanyok.up.railway.app/api/v1'
 };
